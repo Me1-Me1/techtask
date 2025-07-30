1dd5bfc838787a767402968747edb3c0d2fe8fbf
Me1-Me1 <nastasya.molodezheva@gmail.com>
2025-07-30 05:43:30 +0300

    added styles

--- my-app/src/ToDoList.jsx
+++ my-app/src/ToDoList.jsx
@@ -78,7 +78,7 @@ function ToDoList() {
             <ul>
                 {tasks.map((task) => 
                 <li key={task.id}> 
-                <span style={{textDecoration: task.completed ? "line-through" : "none", opacity: task.completed ? 0.7 : 1}}>
+                <span className="task-item" style={{textDecoration: task.completed ? "line-through" : "none", opacity: task.completed ? 0.7 : 1}}>
                     <input 
                         className="done-checkbox"
                         type="checkbox"
